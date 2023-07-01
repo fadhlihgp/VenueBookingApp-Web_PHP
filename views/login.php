fa9958360b4054ff18dcb5fcfd4961a1c8c30060
Fadhlih Girindra Putra <fadhlihgp@gmail.com>
2023-07-01 22:49:19 +0700

    change photo

--- views/login.php
+++ views/login.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/15678448-conceito-de-ilustracao-de-protecao-de-acesso-de-login-uma-ilustracao-plana-isolada-no-fundo-branco-vetor.jpg"
                          class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
